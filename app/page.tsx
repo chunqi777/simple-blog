79b5ef2bb3758e0c45704663cce32cef71cc594b
chunqi777 <LHY2274847317@outlook.com>
2024-01-04 07:39:14 +0800

    style

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-import Clock from "./ui/clock"
 
 export default function Home() {
-  const backgroundImage = '/background.png'
-
   return (
     <div className="flex flex-col">
-      <div className="h-screen w-full bg-cover bg-center bg-no-repeat relative bg-fixed" style={{ backgroundImage: `url(${backgroundImage})` }} >
-        <Clock />
+      <div className="h-screen w-full bg-cover bg-center relative bg-no-repeat bg-fixed bg-[url(/background.png)] before:block before:bg-[url(/dot.gif)] before:h-full before:bg-fixed">
+        <div className="h-full w-full absolute top-0 left-0">
+
+        </div>
       </div>
       <div className="flex h-full w-full bg-slate-50">
-        <div className="bg-white mx-0 w-full md:mx-10" style={{ height: '1000px' }}></div>
+        <div className="bg-white mx-0 w-full md:mx-10 h-[1000px]"></div>
       </div>
     </div>
   )
